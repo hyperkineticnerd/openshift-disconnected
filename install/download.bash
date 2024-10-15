3b1da7ab0b75da0e2ab20c10e0045401a6b0c94c
Sean Nelson <hyperkineticnerd@gmail.com>
2024-10-15 06:32:11 -0700

    Tue Oct 15 06:32:11 PDT 2024

--- install/download.bash
+++ install/download.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-declare CHANNEL=stable-4.12
-declare VERSION=${1:-4.12.36}
+declare CHANNEL=stable-4.16
+declare VERSION=${1:-4.16.14}
 declare OS=${2:-linux}
 
 curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/openshift-install-${OS}-${VERSION}.tar.gz
@@ -9,3 +9,4 @@ curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/op
 curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/oc-mirror.tar.gz
 curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/opm-${OS}-${VERSION}.tar.gz
 curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/ccoctl-${OS}-${VERSION}.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/mirror-registry/1.3.11/mirror-registry.tar.gz
--- mirror/helpers/scripts/push.bash
+++ mirror/helpers/scripts/push.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 declare TOP=${PWD}
-declare target="docker://quay.developer.hkn.lab:8443"
+declare target=${1-"docker://quay.developer.hkn.lab:8443"}
 declare opts="--dest-skip-tls"
 
 function push {
-    echo "Pushing $1 to Container Registry"
-    cd $TOP/$2
+    echo "Pushing ${1} to Container Registry"
+    cd $TOP/${2}
     for mirror_tar in $(ls mirror_*.tar); do
         oc mirror $opts --from $mirror_tar $target
     done
@@ -14,5 +14,5 @@ function push {
 
 push "OpenShift Platform" platform/openshift
 push "Red Hat Operator Index" operators/redhat
-push "Certified Operator Index" operators/certified
-push "Community Operator Index" operators/community
+# push "Certified Operator Index" operators/certified
+# push "Community Operator Index" operators/community
