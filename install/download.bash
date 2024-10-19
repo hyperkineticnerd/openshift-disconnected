6fe9a53afe44236d8cf20849cd402b15e44700bd
Sean Nelson <62039690+hyperkineticnerd@users.noreply.github.com>
2024-10-19 11:40:19 -0600

    Update download.bash

--- install/download.bash
+++ install/download.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-declare CHANNEL=stable-4.16
-declare VERSION=${1:-4.16.14}
-declare OS=${2:-linux}
+declare CHANNEL='stable-4.16'
 
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/openshift-install-${OS}-${VERSION}.tar.gz
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/openshift-client-${OS}-${VERSION}.tar.gz
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/oc-mirror.tar.gz
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/opm-${OS}-${VERSION}.tar.gz
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${VERSION}/ccoctl-${OS}-${VERSION}.tar.gz
-curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/mirror-registry/1.3.11/mirror-registry.tar.gz
+declare VERSION='4.16'
+declare RELEASE=${1:-4.16.14}
+declare OS=${2:-linux}
+declare CPU='amd64'
+declare VARIANT='rhel9'
+
+declare MREGVER='1.3.11'
+declare ROSAVER='1.2.46'
+declare RHCOSVER='4.17.0'
+
+curl -OL https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/${VERSION}/${RHCOSVER}/rhcos-${RHCOSVER}-x86_64-aws.x86_64.vmdk.gz
+
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE}/openshift-install-${OS}-${RELEASE}.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE}/openshift-install-rhel9-amd64.tar.gz
+
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE}/openshift-client-${OS}-${CPU}-${VARIANT}-${RELEASE}.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE}/ccoctl-${OS}-${VARIANT}-${RELEASE}.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE}/opm-${OS}-${RELEASE}.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/rosa/${ROSAVER}/rosa-${OS}.tar.bz
+
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE}/oc-mirror.${VARIANT}.tar.gz
+curl -OL https://mirror.openshift.com/pub/openshift-v4/clients/mirror-registry/${MREGVER}/mirror-registry.tar.gz
